2dfe18d3f98f4d8dff8ce2ad4e15151b18edde92
netanelAmz <33493209+netanelAmz@users.noreply.github.com>
2022-01-18 14:01:18 +0200

    handle permission

--- app/src/main/java/com/example/facerecognition/MainActivity.kt
+++ app/src/main/java/com/example/facerecognition/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.facerecognition
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import com.example.facerecognition.databinding.ActivityMainBinding
@@ -12,15 +15,34 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+    val READ_STORAGE_PERMISSION_REQUEST_CODE: Int = 41
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-         navController = findNavController(R.id.nav_host_fragment_content_main)
+        navController = findNavController(R.id.nav_host_fragment_content_main)
+        if (!checkPermission()) {
+            requestPermissionForReadExtertalStorage()
+        }
     }
 
+    private fun checkPermission(): Boolean {
+        val result = this.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+        return result == PackageManager.PERMISSION_GRANTED
+    }
 
-
+    @Throws(Exception::class)
+    fun requestPermissionForReadExtertalStorage() {
+        try {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                READ_STORAGE_PERMISSION_REQUEST_CODE
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            throw e
+        }
+    }
 
 
     override fun onResume() {
@@ -34,8 +56,6 @@ class MainActivity : AppCompatActivity() {
         DataObject.stateApp = "onPause"
         super.onPause()
     }
-
-
 
 
 }
